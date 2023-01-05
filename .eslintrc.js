06cc7f2693d9d840f8f4d3f3000b1522fe7992da
Chily <cpo@appvestor.com>
2023-01-05 14:03:15 +0100

    Basic structure extended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
-> 1%
-last 2 versions
-not dead
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
